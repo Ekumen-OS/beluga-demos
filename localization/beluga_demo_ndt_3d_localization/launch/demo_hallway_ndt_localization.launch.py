a4b802e09592560cf1c2d5aee1681009a0b21b27
Fer <sanz.fernando172@gmail.com>
2025-02-25 15:08:35 +0100

    Change dates
    Signed-off-by: Fer <sanz.fernando172@gmail.com>

--- localization/beluga_demo_ndt_3d_localization/launch/demo_hallway_ndt_localization.launch.py
+++ localization/beluga_demo_ndt_3d_localization/launch/demo_hallway_ndt_localization.launch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2023 Ekumen, Inc.
+# Copyright 2025 Ekumen, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
